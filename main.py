6fb6f35ed928362133f8cf873b4aa5c1957b8f89
Leandro Tavares <leandro.mendestavares@gmail.com>
2020-01-02 21:49:41 -0300

    'First'

---
+++ main.py
@@ -0,0 +1,11 @@
+import getpass
+
+print("************************************************")
+print("*************** CAIXA ELETRÔNICO ***************")
+print("************************************************")
+
+account_typed = input('Digite sua conta: ')
+password_typed = getpass.getpass('Digite sua senha: ')
+
+print(account_typed)
+print(password_typed)
